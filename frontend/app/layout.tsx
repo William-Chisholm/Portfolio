1b6220ee131c36ddc81c7eb741c349f26ab9ddf1
William-Chisholm <william.chisholm0008@gmail.com>
2025-05-07 00:36:10 -0500

    Re-added frontend as part of repo

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Will Chisholm | Full Stack Developer Portfolio",
+  description: "A full stack web devloper portfolio showcasing modern, performant applicatoins build with Next.js, Flask, SQLAlchemy, Docker, and MSSQL. Explore my work, skills, and contact info.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
